7a3b4a084fc7fb50dbf07851b2da12db9c097c3b
SebastianSroczyk <sroczyk.sebastian@gmail.com>
2024-07-19 02:08:31 +0100

    Working os Assessment

---
+++ Assessments/LetterPyramid.cpp
@@ -0,0 +1,26 @@
+/*
+    Letter Pyramid Assessment
+    Author: Sebastian Sroczyk
+    Date: 19/07/2024
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    
+    
+    return 0;
+}
+
+    
+
+    
+
+    
+
+
+  
