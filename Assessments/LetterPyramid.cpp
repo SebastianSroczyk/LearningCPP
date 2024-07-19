395b893769c37204a5229f21438e6bde21a87a0a
SebastianSroczyk <sroczyk.sebastian@gmail.com>
2024-07-19 18:27:10 +0100

    Failed on a assignment
    I kept getting close to the correct dispaly but I jsut couldn't figure it out. I need to move onto a different project as I spent over 6hours on this

--- Assessments/LetterPyramid.cpp
+++ Assessments/LetterPyramid.cpp
@@ -11,8 +11,32 @@ using namespace std;
 
 int main(){
 
-    
-    
+
+    // Not working properly
+
+    string input {};
+    cout << "Enter the characters you would like to see in your pyramid: ";
+    cin >> input;
+
+    string space (input.length() - 1, ' ');
+    string output{};
+    for( char c: input){
+
+    }
+    for (size_t i {0}; i < input.length(); ++i){   // i: 0,1,2 
+
+        if (i == 0){
+            output = space + input.at(i);
+            cout << output;
+        }
+        else{
+            
+            output = space.erase(0,1) + input.substr(0,i+1);
+            cout << output; 
+        }
+        
+        cout << endl;
+    }
     return 0;
 }
 
