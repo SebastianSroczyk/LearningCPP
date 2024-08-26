116a8ee7ef6a0ef8033bd5094c4ad10eddf0cb49
SebastianSroczyk <sroczyk.sebastian@gmail.com>
2024-08-26 12:22:45 +0100

    Writing notes of OOP

--- Object Oriented Programming/OOPvsProcedural.cpp
+++ Object Oriented Programming/OOPvsProcedural.cpp
@@ -8,6 +8,68 @@
 
 /*
 
+    Procedural Programming
+
+        - Focus is on process or actions a program takes.
+        - Programs are typically a collection of functions.
+        - Data is declared separatly.
+        - Data is passed as arguments into fucntions.
+        - Fairly Easy to learn.
+
+    Procedural Programming Limitations
+
+        - Functions need to know the structure of the data.
+            I.e. If the structure of the data changes many functions will need to be changed.
+
+        - As the program gets larger it becomes:
+            Difficult to understand.
+            Harder to maintain.
+            Difficult to extend.
+            Difficult to Debug.
+            Difficult to reuse code.
+            Fragile and easy to break.
+
+    Object Oriented Programming (OOP)
+
+        - Focus is on Classes; that model the real-world domain entities
+        - Allows developers to think at a higher level of abstraction
+        - Used successfully in larger programs.
+        - Reusability of code.
+
+    4 Pillers of OOP:
+
+        Encapsulation:
+            - Objects contain the data AND operations that use the data
+            - Abstract Data Type (ADT)
+        
+        Abstraction:
+            - Implementation-specific logic can be hidden
+            - User of the class code only interacts with the interface provided and doesn't need to know the code.
+            - More Abstraction
+            - Easier to test, debug, maintain and extend.
+
+        Polymorthism:
+            - Allows classes to create different variations of the same object
+
+        Inheritance:
+            - Allows the creation of a class based on an existing class,
+              which inherits the parents classes data and functions.
+    
+    Limitations of Object Oriented Programming:
+
+        NOT a panacea
+            - OOP will NOT make bad code better
+            - It is NOT suitable for all types of problems
+            - Not everything decomposes into a class
+        
+        Learning Curve.
+
+
+
+
+
+
+
 
 
 */
